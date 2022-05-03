a9ee7f4690f5ed80bc996467ff6b3877d039976d
chahyeonnaa <gusdk21650@naver.com>
2022-05-03 21:21:59 +0900

    [FEAT] 로그인,회원가입,GPS(#1)

--- client/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ client/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button certi_gps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
+
+        certi_gps= (Button) findViewById(R.id.nameadd);
+        certi_gps.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GpsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
