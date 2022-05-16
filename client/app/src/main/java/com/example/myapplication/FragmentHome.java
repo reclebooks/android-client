dbb7a4bcfd5ef29e132e9e77b152b9532a7e436b
Kkang1202 <kkm9814@cbnu.ac.kr>
2022-05-16 15:25:33 +0900

    [FEAT]게시글 작성 xml 연결(#9)

--- client/app/src/main/java/com/example/myapplication/FragmentHome.java
+++ client/app/src/main/java/com/example/myapplication/FragmentHome.java
@@ -1,16 +1,36 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentHome extends Fragment {
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+public class FragmentHome extends Fragment
+{
+    ImageButton writingButton;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        this.writingButton = view.findViewById(R.id.WritingButton);
+        this.writingButton.setOnClickListener(v ->
+        {
+            Intent intent = new Intent(getActivity(), WritingActivity.class);
+            startActivity(intent);
+        });
+        return (view);
     }
 }
